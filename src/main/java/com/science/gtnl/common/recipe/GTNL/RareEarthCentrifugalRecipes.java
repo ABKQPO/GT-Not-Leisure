e9b921aa72a650b182e20b3fe4d0c56c30ee39a2
Psimoo <2243551545@qq.com>
2025-08-17 23:54:56 +0800

    把稀土离心机的输出变成正常粉，输入和配方时间x4

--- src/main/java/com/science/gtnl/common/recipe/GTNL/RareEarthCentrifugalRecipes.java
+++ src/main/java/com/science/gtnl/common/recipe/GTNL/RareEarthCentrifugalRecipes.java
@@ -17,28 +17,28 @@ public class RareEarthCentrifugalRecipes implements IRecipePool {
     @Override
     public void loadRecipes() {
         RecipeBuilder.builder()
-            .itemInputs(MaterialPool.RareEarthMetal.get(OrePrefixes.dust, 1))
+            .itemInputs(MaterialPool.RareEarthMetal.get(OrePrefixes.dust, 4))
             .itemOutputs(
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Lanthanum, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Cerium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Neodymium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Promethium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Samarium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Europium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Praseodymium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Gadolinium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Terbium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Dysprosium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Holmium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Erbium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Thulium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Ytterbium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Scandium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Lutetium, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Yttrium, 1))
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Lanthanum, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Cerium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Neodymium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Promethium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Samarium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Europium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Praseodymium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Gadolinium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Terbium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Dysprosium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Holmium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Erbium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Thulium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Ytterbium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Scandium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Lutetium, 1),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Yttrium, 1))
             .specialValue(0)
             .noOptimize()
-            .duration(200)
+            .duration(800)
             .eut(491520)
             .addTo(RECR);
     }
