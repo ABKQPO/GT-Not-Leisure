968ab23b2c368f9445f5eae63c5d6537e59c979b
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-11-04 11:04:04 +0800

    禁用混淆

--- src/main/java/com/science/gtnl/mixins/late/ThaumicTinkerer/AccessorAspectCropLootManager.java
+++ src/main/java/com/science/gtnl/mixins/late/ThaumicTinkerer/AccessorAspectCropLootManager.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import thaumcraft.api.aspects.Aspect;
 import thaumic.tinkerer.common.core.helper.AspectCropLootManager;
 
-@Mixin(AspectCropLootManager.class)
+@Mixin(value = AspectCropLootManager.class, remap = false)
 public interface AccessorAspectCropLootManager {
 
     @Accessor("lootMap")
