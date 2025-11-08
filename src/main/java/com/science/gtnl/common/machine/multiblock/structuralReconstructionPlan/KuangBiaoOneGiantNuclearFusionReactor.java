6f803accd23bce32dba2cacd5841b9f72f302041
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-11-08 11:42:18 +0800

    Update KuangBiaoOneGiantNuclearFusionReactor.java

--- src/main/java/com/science/gtnl/common/machine/multiblock/structuralReconstructionPlan/KuangBiaoOneGiantNuclearFusionReactor.java
+++ src/main/java/com/science/gtnl/common/machine/multiblock/structuralReconstructionPlan/KuangBiaoOneGiantNuclearFusionReactor.java
@@ -808,6 +808,7 @@ public abstract class KuangBiaoOneGiantNuclearFusionReactor
                 .addInfo(StatCollector.translateToLocal("Tooltip_WirelessEnergyMultiMachine_07"))
                 .addInfo(StatCollector.translateToLocal("Tooltip_WirelessEnergyMultiMachine_08"))
                 .addInfo(StatCollector.translateToLocal("Tooltip_WirelessEnergyMultiMachine_09"))
+                .addInfo(StatCollector.translateToLocal("Tooltip_WirelessEnergyMultiMachine_10"))
                 .addInfo(StatCollector.translateToLocal("Tooltip_Tectech_Hatch"))
                 .addSeparator()
                 .addInfo(StatCollector.translateToLocal("StructureTooComplex"))
