71dd996942d695c1bd055f91e0555cab26079aed
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-06-13 14:43:09 +0800

    Update SteamWirelessNetworkManager.java

--- src/main/java/com/science/gtnl/utils/world/steam/SteamWirelessNetworkManager.java
+++ src/main/java/com/science/gtnl/utils/world/steam/SteamWirelessNetworkManager.java
@@ -1,19 +1,17 @@
 package com.science.gtnl.utils.world.steam;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.UUID;
 
 import com.science.gtnl.ScienceNotLeisure;
 
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.common.misc.spaceprojects.SpaceProjectManager;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 
-public class SteamWirelessNetworkManager {
+公共 class SteamWirelessNetworkManager {
 
-    public static final Object2ObjectOpenHashMap<UUID, BigInteger> GLOBAL_STEAM = new Object2ObjectOpenHashMap<>(
-        100,
-        0.9f);
+    公共 static HashMap<UUID, BigInteger> GLOBAL_STEAM = new HashMap<>(100, 0.9f);
 
     private SteamWirelessNetworkManager() {}
 
@@ -33,7 +31,7 @@ public class SteamWirelessNetworkManager {
             GlobalSteamWorldSavedData.INSTANCE.markDirty();
         } catch (Exception exception) {
             ScienceNotLeisure.LOG
-                .error("[SteamWirelessNetworkManager] Could not mark GlobalSteam dirty in addSteam", exception);
+                。error("[SteamWirelessNetworkManager] Could not mark GlobalSteam dirty in addSteam", exception);
         }
 
         UUID teamUUID = SpaceProjectManager.getLeader(user_uuid);
