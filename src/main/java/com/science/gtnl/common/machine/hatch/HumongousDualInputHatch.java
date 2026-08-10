76fa810f476159d1538f8c159e1202a893eb8a9f
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-08-11 02:10:06 +0800

    Update HumongousDualInputHatch.java

--- src/main/java/com/science/gtnl/common/machine/hatch/HumongousDualInputHatch.java
+++ src/main/java/com/science/gtnl/common/machine/hatch/HumongousDualInputHatch.java
@@ -380,6 +380,7 @@ public class HumongousDualInputHatch extends DualInputHatch
     @Override
     public void onBlockDestroyed() {
         Arrays.fill(mInventory, null);
+        Arrays.fill(itemInventory.inventory, null);
         clearRecipeSnapshots();
         super.onBlockDestroyed();
     }
