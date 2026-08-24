df7f6ab8c054f7c706dfe95f861df2578b7c17b5
LineBroke <i82498127464@gmail.com>
2026-08-24 15:27:38 +0900

    添加签名
    -源质输入仓
    -复合源质输入仓
    -复合源质罐
    -复合源质管道

--- src/main/java/com/science/gtnl/loader/BlockLoader.java
+++ src/main/java/com/science/gtnl/loader/BlockLoader.java
@@ -117,6 +117,11 @@ public class BlockLoader {
         multiEssentiaInputHatch = new BlockMultiEssentiaInputHatch();
         multiEssentiaJar = new BlockMultiEssentiaJar();
         multiEssentiaTube = new BlockMultiEssentiaTube();
+        AnimatedTooltipHandler.addItemTooltip(GTNLItemList.EssentiaHatch.get(1), AnimatedText.SCIENCE_NOT_LEISURE);
+        AnimatedTooltipHandler
+            .addItemTooltip(GTNLItemList.MultiEssentiaInputHatch.get(1), AnimatedText.SCIENCE_NOT_LEISURE);
+        AnimatedTooltipHandler.addItemTooltip(GTNLItemList.MultiEssentiaJar.get(1), AnimatedText.SCIENCE_NOT_LEISURE);
+        AnimatedTooltipHandler.addItemTooltip(GTNLItemList.MultiEssentiaTube.get(1), AnimatedText.SCIENCE_NOT_LEISURE);
         enderElevatorBlock = new BlockEnderElevator(0);
         enderElevatorSlab = new BlockEnderElevator(1);
         enderElevatorCarpet = new BlockEnderElevator(2);
