b2fed00dd3e8a1b59514ab01160026cd2b7cef45
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-09-01 22:14:57 +0800

    添加便携元件工作台

---
+++ src/main/java/com/science/gtnl/common/item/items/PortableCellWorkbenchItem.java
@@ -0,0 +1,36 @@
+package com.science.gtnl.common.item.items;
+
+import static com.science.gtnl.ScienceNotLeisure.RESOURCE_ROOT_ID;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import com.science.gtnl.CommonProxy;
+import com.science.gtnl.client.GTNLCreativeTabs;
+import com.science.gtnl.utils.enums.GTNLItemList;
+import com.science.gtnl.utils.enums.GuiType;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class PortableCellWorkbenchItem extends Item {
+
+    public PortableCellWorkbenchItem() {
+        setUnlocalizedName("PortableCellWorkbench");
+        setTextureName(RESOURCE_ROOT_ID + ":PortableCellWorkbench");
+        setMaxStackSize(1);
+        setCreativeTab(GTNLCreativeTabs.GTNotLeisureItem);
+        GameRegistry.registerItem(this, getUnlocalizedName());
+        GTNLItemList.PortableCellWorkbench.set(new ItemStack(this));
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        if (!world.isRemote) {
+            CommonProxy
+                .openGui(player, GuiType.PortableCellWorkbenchGUI, null, world, player.inventory.currentItem, 0, 0);
+        }
+        return stack;
+    }
+}
