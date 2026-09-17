2f79569feef30b2f5f211f2941a0e82ccbe6e753
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-09-17 12:56:05 +0800

    feat: 小型奥术组装机
    新增小型奥术组装机，处理有序与无序奥术工作台配方。
    
    - SmallArcaneAssembler 多方块 + 法杖魔力充能机制
    - ShapedArcaneCraftingRecipes 改用 TCRecipeTools
    - AssemblerRecipes: 机器本体装配配方
    - 工业奥术装配室改用注魔 NEI frontend

--- src/main/java/com/science/gtnl/loader/MachineLoader.java
+++ src/main/java/com/science/gtnl/loader/MachineLoader.java
@@ -114,6 +114,7 @@ import com.science.gtnl.common.machine.multiblock.ResearchCenter;
 import com.science.gtnl.common.machine.multiblock.ResourceCollectionModule;
 import com.science.gtnl.common.machine.multiblock.ShallowChemicalCoupling;
 import com.science.gtnl.common.machine.multiblock.SingularityDataHub;
+import com.science.gtnl.common.machine.multiblock.SmallArcaneAssembler;
 import com.science.gtnl.common.machine.multiblock.SmallInfusionMatrix;
 import com.science.gtnl.common.machine.multiblock.SpaceAssembler;
 import com.science.gtnl.common.machine.multiblock.SuperSpaceElevator;
@@ -469,6 +470,14 @@ public class MachineLoader {
                 ""));
         AnimatedTooltipHandler
             .addItemTooltip(GTNLItemList.IndustrialArcaneAssembler.get(1), AnimatedText.SCIENCE_NOT_LEISURE);
+
+        GTNLItemList.SmallArcaneAssembler.set(
+            new SmallArcaneAssembler(
+                GTNLMachineID.SMALL_ARCANE_ASSEMBLER.ID,
+                "SmallArcaneAssembler",
+                StatCollector.translateToLocal("gtnl.machine.small_arcane_assembler.name")));
+        AnimatedTooltipHandler
+            .addItemTooltip(GTNLItemList.SmallArcaneAssembler.get(1), AnimatedText.SCIENCE_NOT_LEISURE);
 
         GTNLItemList.SmallInfusionMatrix.set(
             new SmallInfusionMatrix(
--- src/main/java/com/science/gtnl/utils/enums/GTNLItemList.java
+++ src/main/java/com/science/gtnl/utils/enums/GTNLItemList.java
@@ -490,6 +490,7 @@ public enum GTNLItemList implements IItemContainer {
     MultiEssentiaJar,
     MultiEssentiaTube,
     MultiEssentiaInputHatch,
+    SmallArcaneAssembler,
     SmallInfusionMatrix,
     EternalGregTechWorkshop,
     EGTWFusionModule,
