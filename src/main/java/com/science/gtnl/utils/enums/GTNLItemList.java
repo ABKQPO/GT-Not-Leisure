801e8013d906604f76e3ea9201c5fba04ab2ef16
LineBroke <i82498127464@gmail.com>
2026-09-19 15:59:14 +0900

    feat: 复合源质输入仓 (#605)
    新增复合源质输入仓，可从相邻源质罐/管道自动吸取多种源质供多方块使用。
    
    - Block/Item/TileEntity: MultiEssentiaInputHatch
    - 接入神秘能源 (Thaumic Energistics) 注入权限
    - 空手潜行右击清空全部源质
    
    Co-authored-by: ABKQPO <93412322+ABKQPO@users.noreply.github.com>

--- src/main/java/com/science/gtnl/utils/enums/GTNLItemList.java
+++ src/main/java/com/science/gtnl/utils/enums/GTNLItemList.java
@@ -489,6 +489,7 @@ public enum GTNLItemList implements IItemContainer {
     StellarIrisController,
     MultiEssentiaJar,
     MultiEssentiaTube,
+    MultiEssentiaInputHatch,
     EternalGregTechWorkshop,
     EGTWFusionModule,
     ETGWEyeOfHarmonyModule,
